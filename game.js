e10237562056e94fa714133d36d4d2ff7c12ed63
thar12345 <tharsihan019@gmail.com>
2023-05-23 17:45:00 -0400

    Add sound playback and button animation

--- game.js
+++ game.js
@@ -17,6 +17,8 @@ $(document).keydown(function() {
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -31,4 +33,16 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour)
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();  
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () { $("#" + currentColour).removeClass("pressed")}, 100);
 }
